3d85a57b760cc1e708f73ee1c4076bca2b8b8d0b
James Segarra III <JamesSegarra@gmail.com>
2016-11-21 15:10:35 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,3 +1,22 @@
 export default function createStore(reducer){
-// your code here!
+  let state;
+  let listeners = [];
+
+  function getState(){
+    return state || reducer(undefined,{type:'default'});
+  }
+
+  function subscribe(callback){
+    callback();
+  }
+
+  function dispatch(action) {
+    state = reducer(state,action);
+  }
+
+  return {
+    getState: getState,
+    subscribe: subscribe,
+    dispatch: dispatch
+  }
 }
